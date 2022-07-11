5c115bfe10f7706baaac6759fcb863bce7e3593a
Aleksandr <88761218+RedHeat95@users.noreply.github.com>
2022-07-11 21:43:34 +0300

    pattern

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import './App.css';
+import { Container } from 'react-bootstrap';
+import { Header } from './components/Header/Header';
+import { Main } from './components/Main/Main';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Container>
+      <Header />
+      <Main />
+    </Container>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,41 @@
+import {
+  Navbar,
+  Container,
+  Nav,
+  Form,
+  FormControl,
+  Image,
+} from 'react-bootstrap';
+
+export const Header = () => {
+  return (
+    <>
+      <Navbar bg="light" className="mb-3">
+        <Container>
+          <Navbar.Brand href="/">
+            <h1>LOGO</h1>
+          </Navbar.Brand>
+
+          <div>
+            <Nav className="me-3">
+              <Nav.Link href="/">Topic 1</Nav.Link>
+              <Nav.Link href="/">Topic 2</Nav.Link>
+              <Nav.Link href="/">Topic 3</Nav.Link>
+              <Nav.Link href="/">Topic 4</Nav.Link>
+            </Nav>
+
+            <Form className="d-flex justify-content-end">
+              <div className="d-flex bg-white ">
+                <FormControl
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+              </div>
+            </Form>
+          </div>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
